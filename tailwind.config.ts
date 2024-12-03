8061c67d7e9fde43c23d8184f6e98fce5009cc28
PerryFinn <Chen-913@users.noreply.github.com>
2024-12-03 21:04:31 +0800

    feat: 添加 Divider、Link、Heading、Subheading、DescriptionList、Text、Textarea 组件，更新 Tailwind 配置，新增 @headlessui/react 依赖

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,6 +67,9 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out"
+      },
+      fontFamily: {
+        sans: ["Inter, sans-serif", { fontFeatureSettings: '"cv11"' }]
       }
     }
   },
