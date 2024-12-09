8d958bc9d0868a6dd7cc10b639acfb794c1c3bed
PerryFinn <Chen-913@users.noreply.github.com>
2024-12-09 14:10:47 +0800

    feat: 添加 LinkPreview 组件，集成 HoverCard，优化设置页面展示

--- src/options/routes/settings/base/index.tsx
+++ src/options/routes/settings/base/index.tsx
@@ -1,9 +1,22 @@
+import { LinkPreview } from "@/components/complex-ui/link-preview";
 import { createFileRoute } from "@tanstack/react-router";
+import windowConfigPreviewPng from "assets/previews/dws-window-config.png";
+import { CircleHelp } from "lucide-react";
 
 export const Route = createFileRoute("/settings/base/")({
   component: RouteComponent
 });
 
 function RouteComponent() {
-  return <div>Hello "/settings/base/"!</div>;
+  return (
+    <div className="flex">
+      <LinkPreview
+        isStatic
+        // imageSrc="https://pro-ali-dws.cvtestatic.com/dws-model/uwiwjuvohjxyjhnuhoyjypwkymhhihhh.png"
+        imageSrc={windowConfigPreviewPng}
+      >
+        <CircleHelp />
+      </LinkPreview>
+    </div>
+  );
 }
